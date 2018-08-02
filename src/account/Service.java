c2edc56c4bdeb87775278dc00cd605a021d34dc1
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2018-08-02 12:02:34 +0100

    Add files via upload

--- src/account/Service.java
+++ src/account/Service.java
@@ -1,17 +1,22 @@
 package account;
-
 import java.util.HashMap;
 
+import org.json.JSONObject;
+
 public class Service {
-	
-	HashMap <Integer, Account> person = new HashMap<>();
+
+	private HashMap <Integer, Account> person = new HashMap<>();
 	
 	public void getAccount(Account account) {
 		person.put(account.getAccountN(),account); 
 	}
 	
 	public Account collectAccount(int id) {
-		return person.get(id); 
-	}	 
+		return person.get(id);
+	}
+	public JSONObject mapToJson() {
+		
+		return new JSONObject(person);
+	}
 
 }
