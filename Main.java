1fd6b472c75c72c37b63ff6867c8183651ac7dbf
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2018-08-02 11:55:02 +0100

    Files uploaded
    Files would not commit from eclipse.

---
+++ Main.java
@@ -0,0 +1,14 @@
+package account;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Service service = new Service();
+		Account account = new Account("Kris", "Duff",3);
+		service.getAccount(account);
+		System.out.println(service.mapToJson());
+	}
+
+}
+ 
