ecdbbd25085539077ea1b51408bd010d2a9ec772
KrisDuff456 <41322885+KrisDuff456@users.noreply.github.com>
2018-08-03 10:52:59 +0100

    Commite to master

--- src/account/Main.java
+++ src/account/Main.java
@@ -5,11 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Service service = new Service();
-		Account account = new Account("Kris", "Duff", 3);
+		Account account = new Account("Kris", "Duff",3);
 		service.getAccount(account);
-		System.out.println(service.name("Kris"));
-		 
-	} 
+		System.out.println(service.mapToJson());
+	}
 
 }
   
